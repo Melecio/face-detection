feebb59252a7c1aaff0d3ebf0881238c3f167300
Gabriel Formica <gabrielformica93@gmail.com>
2014-11-28 12:11:07 -0430

    se termina de planificar la estructura

--- script.py
+++ script.py
@@ -4,6 +4,9 @@
 import sys
 import argparse
 from PIL import Image, ImageFilter
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.tools.xml.networkwriter import NetworkWriter
+from pybrain.tools.xml.networkreader import NetworkReader
 
 """Ranks a tuple (r,g,b) between 0 and 16777215"""
 def rank( (r, g, b) ):
@@ -27,10 +30,25 @@ def get_features(img):
             features.append(v)     # appending every pixel 
 
 """Given list of images (data_set), trains the network with backpropagation algorithm"""
-def train_brain(data_set):
-    for img in data_set:
-        inputs = get_features(img)
-        #call backprop inputs
+def train_brain(net, input_set):
+    for img in input_set:
+        features = get_features(img)
+        # call backprop features
+
+
+
+def test_brain(net, img):
+    (width, height) = img.size
+    i = 0
+    while (i < height):
+        j = 0
+        while (j < width):
+            features = get_window(img, i, j)
+            #call backprop features  
+            j += 10
+        i += 10
+
+
 
 
 """Returns a list of images"""
@@ -45,7 +63,26 @@ def main():
     parser.add_argument('-t', '--train', help='Receives a list of images (training set)', nargs='+')
     parser.add_argument('-p', '--test', help='Receives a list of images (testing set)', nargs='+')
     parser.add_argument('-n', '--network', help='Read the file with the already trained network object', nargs=1)
+
     args = parser.parse_args()
+    net = buildNetwork(2, 4, 1)
+
+    if (args.network != None):       # No need to train. Just read the Neura Network Object
+        file_object = open(args.network, 'r')
+        net = NetworkReader.readFrom(file_object)
+    else:
+        if (args.train != None):
+            training_set = get_data_set(args.train)
+            train_brain(training_set)
+
+    if (args.test != None):
+        testing_set = get_data_set(args.test)
+
+
+
+
+            
+        
 
 
 if __name__ == "__main__":
