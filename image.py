832ac3b446159592d4a94f0d481fcf37ed617b79
Matteo Ferrando <matteo.ferrando2@gmail.com>
2014-11-28 15:47:25 -0430

    Finished image proecessing. The training set is 19x19 images, that doesn't work.

---
+++ image.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from PIL import Image
+
+"""Ranks a pixel of an img, either in RGB, in L or in LA mode"""
+def pixel_rank(img, pixel):
+    if img.mode == 'RGB':
+        (r,g,b) = img.getpixel(pixel)
+        return int(round((r + g + b) / 3))
+    elif img.mode == 'L':
+        return img.getpixel(pixel)
+    elif img.mode == 'LA':
+        l, _ = img.getpixel(pixel)
+        return l
+
+"""Returns a 20x20 window given the left and top coordinates"""
+def img_window(img, i, j):
+    width, height = img.size
+
+    if width < i + 20:
+        i = width - 20
+    if height < j + 20:
+        j = height - 20
+
+    box = (i, j, i+20, j+20)
+    return img.crop(box)
+
+
+"""
+Returns a vector of a linearly ranked value of img
+This function should be called with a 20x20 pixels img (window)
+"""
+def img_features(img):
+    width, height = img.size
+    vector = []
+    for i in range(width):
+        for j in range(height):
+            ranked = pixel_rank(img, (i,j))
+            vector.append(ranked)
+    return vector
+
+"""
+Crop an image in 20x20 squares and yield each result
+"""
+def img_crops(img):
+    width, height = img.size
+    top = 0
+
+    # Padding for taking every pixel into account
+    if (width % 5) != 0:
+        width += 5 - (width % 5) - 20
+    else:
+        width -= 20
+
+    if (height % 5) != 0:
+        height += 5 - (height % 5) - 20
+    else:
+        height -= 20
+
+    while top <= height:
+        left = 0
+        while left <= width:
+            yield img_window(img, left, top)
+            left += 5
+        top += 5
+
+"""
+Get all the windows of a given image to pass to the neural network
+"""
+def img_windows(img):
+    width, height = img.size
+    while(width > 20 and height > 20):
+        for crop in img_crops(img):
+            yield crop
+
+        width  = int(round(width / 1.2))
+        height = int(round(height / 1.2))
+        img = img.resize((width, height), Image.ANTIALIAS)
+
+"""
+Get all the representing vectors of the windows of a given image
+"""
+def img_features_vectors(img):
+    print "img_features_vectors"
+    for window in img_windows(img):
+        print "window"
+        yield img_features(window)
---
+++ neural_network.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import argparse
+
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.tools.xml.networkwriter import NetworkWriter
+from pybrain.tools.xml.networkreader import NetworkReader
+
+from PIL import Image
+
+# Module image in 'image.py'
+from image import img_features_vectors, img_features
+
+"""Given list of images, trains the network with the backpropagation algorithm"""
+def train_brain(net, images):
+    for img in images:
+        vector = img_features(img)
+        print vector
+        # call backprop vector
+
+"""Given list of images, test the network with the backpropagation algorithm"""
+def test_brain(net, images):
+    for img in images:
+        for vector in img_features_vectors(img):
+            print vector
+            # call backprop vector
+
+"""Opens the images of the data set"""
+def get_data_set(files):
+    for img in files:
+        yield Image.open(img)
+
+def main():
+    parser = argparse.ArgumentParser(description='Face detection using Neural Networks')
+    parser.add_argument('-t', '--train', help='Receives a list of images (training set)', nargs='+')
+    parser.add_argument('-p', '--test', help='Receives a list of images (testing set)', nargs='+')
+    parser.add_argument('-n', '--network', help='Read the file with the already trained network object', nargs=1)
+
+    args = parser.parse_args()
+
+    # No need to train. Just read the Neura Network Object
+    if (args.network != None):
+        print "net"
+        file_object = open(args.network, 'r')
+        net = NetworkReader.readFrom(file_object)
+    else:
+        print "no net"
+        net = buildNetwork(400, 20, 1)
+
+    if (args.train != None):
+        print "train"
+        training_set = get_data_set(args.train)
+        train_brain(net, training_set)
+
+    if (args.test != None):
+        print "test"
+        testing_set = get_data_set(args.test)
+        test_brain(net, testing_set)
+
+if __name__ == "__main__":
+    main()
